619224a04a70d0ad66bb5e0bcfdbe34662436fac
jazz511 <46452663+jazz511@users.noreply.github.com>
2019-08-18 12:23:39 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+echo "const botConfig = {
+ db: {
+   host: 'localhost',
+   port: 27017,
+   name: 'test'
+ },
+ token: '872551442:AAEIIaNdhNM9Nb60THLK0KFFa25puXXl7hk', //Your bot token
+};
+
+module.exports = botConfig;" > conig.fs
+
+npm install
+
+node bot
