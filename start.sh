4532f4b3ccd7de9e4a304722571c93e4ccf7a4d8
jazz511 <46452663+jazz511@users.noreply.github.com>
2019-08-18 13:10:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,23 @@ echo "const botConfig = {
  token: '872551442:AAEIIaNdhNM9Nb60THLK0KFFa25puXXl7hk', //Your bot token
 };
 
-module.exports = botConfig;" > config.js 
-sudo apt install subversion
-svn checkout https://github.com/Bannerets/tdlib-binaries/trunk/1.2.0/linux64 ./
+module.exports = botConfig;" > config.js
 
+    sudo apt-get update
+    sudo apt-get upgrade
+    sudo apt-get install make git zlib1g-dev libssl-dev gperf php cmake g++
+    git clone https://github.com/tdlib/td.git
+    cd td
+    rm -rf build
+    mkdir build
+    cd build
+    export CXXFLAGS=""
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
+    cmake --build . --target install
+    cd ..
+    cd ..
+    ls -l td/tdlib
+    ls -l /usr/local
 ls -a
 
 npm install
